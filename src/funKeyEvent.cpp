c90d8bdb81755a589fb16574ffe61c1ab623343f
骏烨 曹 <faye542@outlook.com>
2023-11-05 23:44:50 +0800

    Event Rewrite for HotKey setting.

--- src/funKeyEvent.cpp
+++ src/funKeyEvent.cpp
@@ -7,26 +7,59 @@ void MainWindow::keyPressEvent(QKeyEvent * ev)
     {
         ui->btn_pause->click();
     }
-    else if (ev->key()==Qt::Key_Right){
+    else if (ev->key()==Qt::Key_Right && ev->modifiers() == (Qt::AltModifier))
+    {
+        emit ui->btn_next->signalListJumpPlay();
+    }
+    else if (ev->key()==Qt::Key_Left && ev->modifiers() == (Qt::AltModifier))
+    {
+        emit ui->btn_pre->signalListJumpPlay();
+    }
+    else if (ev->key()==Qt::Key_Right)
+    {
         ui->btn_next->click();
     }
-    else if (ev->key()==Qt::Key_Left){
+    else if (ev->key()==Qt::Key_Left)
+    {
         ui->btn_pre->click();
     }
-    else if (ev->key()==Qt::Key_O){
+    else if (ev->key()==Qt::Key_Up)
+    {
+        int val = ui->slider_volume->value();
+        if(val>=95) {val=100;}
+        else {val+=5;}
+        ui->slider_volume->setValue(val);
+    }
+    else if (ev->key()==Qt::Key_Down)
+    {
+        int val = ui->slider_volume->value();
+        if(val<=5) {val=0;}
+        else {val-=5;}
+        ui->slider_volume->setValue(val);
+    }
+    else if (ev->key()==Qt::Key_O)
+    {
         ui->btn_open->click();
     }
-    else if (ev->key()==Qt::Key_Delete){
+    else if (ev->key()==Qt::Key_Delete)
+    {
         ui->btn_delete->click();
     }
-    else if (ev->key()==Qt::Key_L){
+    else if (ev->key()==Qt::Key_L)
+    {
         ui->btn_list->click();
     }
-    else if (ev->key()==Qt::Key_X){
+    else if (ev->key()==Qt::Key_X)
+    {
         ui->spinBox_pbr->setValue(ui->spinBox_pbr->value()-0.25);
     }
-    else if (ev->key()==Qt::Key_C){
+    else if (ev->key()==Qt::Key_C)
+    {
         ui->spinBox_pbr->setValue(ui->spinBox_pbr->value()+0.25);
+    }
+    else if (ev->key()==Qt::Key_M)
+    {
+        ui->btn_sound->click();
     }
     else
     {
